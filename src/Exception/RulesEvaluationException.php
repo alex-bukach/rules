73afad43e7075937d2add788f3bea73e8806c22a
Anton MArtyniuk <toxaviking@gmail.com>
2016-05-13 11:01:57 +0300

    base commit

---
+++ src/Exception/RulesEvaluationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\rules\Exception;
+
+/**
+ * An exception that is thrown during evaluation.
+ */
+class RulesEvaluationException extends RulesException {
+
+}
---
+++ src/Exception/RulesIntegrityException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\rules\Exception;
+
+/**
+ * An exception that is thrown during integrity checks at configuration time.
+ */
+class RulesIntegrityException extends RulesException {
+
+}
